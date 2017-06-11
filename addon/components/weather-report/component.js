83c7d0582378095f555e829c539b53fb0941d1c5
VinodhRaja Muralidharan <vinodhraja.muralidharan@cognizant.com>
2017-06-11 21:41:41 +0100

    weather

--- addon/components/weather-report/component.js
+++ addon/components/weather-report/component.js
@@ -3,8 +3,9 @@ export default Ember.Component.extend({
   ajax: Ember.inject.service(),
   init() {
     this._super(...arguments);
-    let report = this.get('ajax').request('http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=6abc934e4f0ae1a25db0a39ff02fc276');
-    this.set('report', report);
-    debugger;
+    this.get('ajax').request('http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=6abc934e4f0ae1a25db0a39ff02fc276')
+      .then((resp) => {
+        this.set('report', resp.list);
+      });
   }
 });
